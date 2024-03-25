64b0eb9f00d6bd188f2c85888ef1919b3bd82373
JakubHanzalik <59615996+JakubHanzalik@users.noreply.github.com>
2024-03-25 22:01:40 +0100

    Rozrobene

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AngularController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-//Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
+Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
