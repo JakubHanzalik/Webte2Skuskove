6ffd8a5561b21b3536c3f7e54e95912fe3e7f28c
Jakub Hanzalik <jakub.hanzalik@gmail.com>
2024-04-30 17:47:08 +0200

    Spraveny controller pre questions a swagger

--- backend/controllers/QuestionsController.php
+++ backend/controllers/QuestionsController.php
@@ -5,10 +5,14 @@ namespace Stuba\Controllers;
 use OpenApi\Attributes as OA;
 use Pecee\SimpleRouter\SimpleRouter;
 use Stuba\Handlers\JwtHandler;
+use Stuba\Models\Questions\CreateQuestion\CreateQuestionResponseModel;
 use Stuba\Models\Questions\GetAllQuestions\GetQuestionsResponseModel;
 use Stuba\Models\Questions\GetQuestion\GetQuestionResponseModel;
 use Stuba\Models\Questions\AnswerModel;
 use Stuba\Models\Questions\EQuestionType;
+use Stuba\Models\Questions\UpdateQuestion\UpdateQuestionRequestModel;
+use Stuba\Models\Questions\CreateQuestion\CreateQuestionRequestModel;
+
 
 #[OA\Tag('Question')]
 class QuestionsController
@@ -48,11 +52,11 @@ class QuestionsController
         ])->httpCode(200);
     }
 
-    #[OA\Get(path: '/api/question/{id}', description: "Get question by id", tags: ['Question'])]
-    #[OA\Parameter(name: "id", in: 'path', required: true, description: "Question code", example: "5", schema: new OA\Schema(type: 'string'))]
+    #[OA\Get(path: '/api/question/{code}', description: "Get question by code", tags: ['Question'])]
+    #[OA\Parameter(name: "code", in: 'path', required: true, description: "Question code", example: "abcde", schema: new OA\Schema(type: 'string'))]
     #[OA\Response(response: 200, description: 'Get question by id of logged user', content: new OA\JsonContent(ref: '#/components/schemas/GetQuestionResponseModel'))]
     #[OA\Response(response: 401, description: 'Unauthorized')]
-    public function getQuestionById(int $id)
+    public function getQuestionById(string $code)
     {
         $accessToken = $_COOKIE["AccessToken"];
         $username = $this->jwtHandler->decodeAccessToken($accessToken)["sub"];
@@ -85,10 +89,50 @@ class QuestionsController
         ])->httpCode(200);
     }
 
-    public function createQuestion()
+    #[OA\Post(path: '/api/question/{code}', tags: ['Question'])]
+    #[OA\Parameter(name: "code", in: 'path', required: true, description: "Question code", example: "abcde", schema: new OA\Schema(type: 'string'))]
+    #[OA\RequestBody(description: 'Update question', required: true, content: new OA\JsonContent(ref: '#/components/schemas/UpdateQuestionRequestModel'))]
+    #[OA\Response(response: 200, description: 'Update question')]
+    #[OA\Response(response: 401, description: 'Unauthorized')]
+    public function updateQuestion(string $code)
     {
         $accessToken = $_COOKIE["AccessToken"];
         $username = $this->jwtHandler->decodeAccessToken($accessToken)["sub"];
 
+        $model = new UpdateQuestionRequestModel(SimpleRouter::request()->getInputHandler()->all());
+
+        //TODO: Aktualizovat otazku v databaze
+
+        SimpleRouter::response()->httpCode(200);
+    }
+
+    #[OA\Put(path: '/api/question', tags: ['Question'])]
+    #[OA\RequestBody(description: 'Create question', required: true, content: new OA\JsonContent(ref: '#/components/schemas/CreateQuestionRequestModel'))]
+    #[OA\Response(response: 200, description: 'Create question', content: new OA\JsonContent(ref: '#/components/schemas/CreateQuestionResponseModel'))]
+    #[OA\Response(response: 401, description: 'Unauthorized')]
+    public function createQuestion()
+    {
+        $model = new CreateQuestionRequestModel(SimpleRouter::request()->getInputHandler()->all());
+
+        //TODO: Vytvorit otazku v databaze
+
+        //Mock data
+        SimpleRouter::response()->json([
+            new CreateQuestionResponseModel([
+                "code" => "abcde",
+            ])
+        ])->httpCode(200);
+    }
+
+    #[OA\Delete(path: '/api/question/{code}', tags: ['Question'])]
+    #[OA\Parameter(name: "code", in: 'path', required: true, description: "Question code", example: "abcde", schema: new OA\Schema(type: 'string'))]
+    #[OA\Response(response: 200, description: 'Delete question')]
+    #[OA\Response(response: 401, description: 'Unauthorized')]
+    public function deleteQuestion(string $code)
+    {
+        $accessToken = $_COOKIE["AccessToken"];
+        $username = $this->jwtHandler->decodeAccessToken($accessToken)["sub"];
+
+        SimpleRouter::response()->httpCode(200);
     }
 }
