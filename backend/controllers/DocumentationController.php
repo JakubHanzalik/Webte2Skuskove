29312e731b1fad249d0d8f3d7f48a5ddfae51cdc
Jakub Hanzalik <jakub.hanzalik@gmail.com>
2024-05-04 12:23:07 +0200

    Funguje generovanie prirucky

--- backend/controllers/DocumentationController.php
+++ backend/controllers/DocumentationController.php
@@ -4,20 +4,38 @@ namespace Stuba\Controllers;
 
 use OpenApi\Attributes as OA;
 use Pecee\SimpleRouter\SimpleRouter;
-use Knp\Snappy\Pdf;
+use HeadlessChromium\BrowserFactory;
+use HeadlessChromium\Page;
 
 #[OA\Tag('Documentation')]
 class DocumentationController
 {
+    private BrowserFactory $browserFactory;
+
+    public function __construct()
+    {
+        $this->browserFactory = new BrowserFactory();
+        $this->browserFactory->addOptions(['ignoreCertificateErrors' => true]);
+    }
+
     #[OA\Get(path: '/api/docs', tags: ['Documentation'], description: 'Generate User manual')]
     #[OA\Response(response: 200, description: 'Generate API documentation', content: new OA\MediaType(mediaType: 'application/pdf'))]
     public function generateDocs()
     {
-        $snappy = new Pdf('/usr/bin/wkhtmltopdf');
-        $snappy->setOption('print-media-type', true);
-        header('Content-Type: application/pdf');
+        $browser = $this->browserFactory->createBrowser();
 
-        echo $snappy->getOutput('https://sympli.io/blog/a-quick-guide-to-css-for-printable-webpages');
+        try {
+            $page = $browser->createPage();
+            $page->navigate('https://web/tutorial')->waitForNavigation(PAGE::FIRST_CONTENTFUL_PAINT, 10000);
+
+            $page->pdf(['printBackground' => true])->saveToFile("/tmp/webte/user-manual.pdf");
+
+            header('Content-Type: application/pdf');
+            readfile("/tmp/webte/user-manual.pdf");
+        } finally {
+            $browser->close();
+        }
+
         SimpleRouter::response()->httpCode(200);
     }
 }
