61a312d81dc36e6e41f3d8f42a5872d50114f15e
Alexandra Hirnakova <saska7891@gmail.com>
2024-05-05 15:23:14 +0200

    validacie

--- backend/models/User/UpdateUser/UpdateUserRequestModel.php
+++ backend/models/User/UpdateUser/UpdateUserRequestModel.php
@@ -5,6 +5,8 @@ namespace Stuba\Models\User\UpdateUser;
 use JsonSerializable;
 use OpenApi\Attributes as OA;
 use Stuba\Models\User\EUserRole;
+use Respect\Validation\Validator as v ;
+use Respect\Validation\Exceptions\NestedValidationException;
 
 #[OA\Schema(type: 'object', title: 'UpdateUserRequestModel')]
 class UpdateUserRequestModel implements JsonSerializable
@@ -23,10 +25,16 @@ class UpdateUserRequestModel implements JsonSerializable
 
     #[OA\Property(title: 'type', type: 'integer', enum: EUserRole::class)]
     public EUserRole $role;
+    private $validator;
 
     public function __construct()
     {
         unset($this->role);
+        $this->validator = v::attribute('username', v::stringType()->notEmpty()->length(3, 255))
+            ->attribute('password', v::stringType()->notEmpty())
+            ->attribute('name', v::stringType()->notEmpty())
+            ->attribute('surname', v::stringType()->notEmpty())
+            ->attribute('role', v:: instanceOf(EUserRole::class));
     }
 
     public function __set($key, $value)
@@ -34,6 +42,20 @@ class UpdateUserRequestModel implements JsonSerializable
         if ($key === 'role') {
             $this->role = EUserRole::from($value);
         }
+    }
+    public function isValid(): bool
+    {
+        return $this->validator->validate($this);
+    }
+
+    public function getErrors(): array
+    {
+        try {
+            $this->validator->assert($this);
+        } catch (NestedValidationException $exception) {
+            return $exception->getMessages();
+        }
+        return [];
     }
 
     public static function createFromModel($user): UpdateUserRequestModel
