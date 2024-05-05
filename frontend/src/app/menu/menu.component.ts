8298a16e576b6c2c0fec6d1d5ac8259b4ab9096b
Bubino37 <2mega.3mega@gmail.com>
2024-05-05 21:37:06 +0200

    uprava menu

--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [
     RouterOutlet,
-    RouterModule
+    RouterModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
    ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
